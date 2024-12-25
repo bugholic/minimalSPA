57dd556fe5ae1715529473ecf4fe01986f49e0a0
bugholic <masterav2001@gmail.com>
2024-12-26 01:27:48 +0530

    completed part 1

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import image3 from "@/assets/images/project-3.jpg";
 import image4 from "@/assets/images/project-4.jpg";
 import image5 from "@/assets/images/project-5.jpg";
 import { a, div } from "motion/react-client";
+import Image from "next/image";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const projects = [
@@ -32,31 +33,38 @@ const projects = [
 
 const Projects: FC = () => {
   return (
-    <section>
+    <section className="section">
       <div className="container">
-        <h2 className="text-4xl">Selected Works</h2>
-        <div className="mt-10">
+        <h2 className="text-4xl md:text-7xl lg:text-8xl">Selected Works</h2>
+        <div className="mt-10 md:mt-16">
           {projects.map(({ name, image }) => (
             <a href="#" key={name}>
-              <div className="border-t border-stone-100 py-6"></div>
-              <div>
-                <img src={image.src} alt={`${name} Image`} className="w-full" /> 
-                <div className="mt-8 flex justify-between">
-                  <h3 className="text-2xl">{name}</h3>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              <div className="border-t last:border-b border-dotted flex flex-col border-stone-400 py-6 md:py-8 lg:py-10">
+                <div>
+                  <div className="aspect-video md:hidden">
+                    <Image
+                      src={image}
+                      alt={`${name} Image`}
+                      className="w-full"
                     />
-                  </svg>
+                  </div>
+                  <div className="mt-8 flex justify-between items-center md:mt-0">
+                    <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                      />
+                    </svg>
+                  </div>
                 </div>
               </div>
             </a>
