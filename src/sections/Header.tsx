da7d847f33a225ed84a453ce7be38dc078f2b48d
bugholic <masterav2001@gmail.com>
2024-12-30 23:30:06 +0530

    Header animation

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button } from "@/components/Button";
+import { motion, useAnimate } from "motion/react";
 import { header } from "motion/react-client";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -27,34 +29,98 @@ const navItems = [
 ];
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+
+  useEffect(() => {
+    if (isOpen) {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45,
+          },
+        ],
+      ]);
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45,
+          },
+        ],
+      ]);
+    }
+  }, [isOpen]);
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-      <div className="container !max-w-full">
-        <div className="flex justify-between h-20 items-center">
-          <div>
-            <a href="/">
-              <span className="text-xl font-bold uppercase">
-                Alex&nbsp; Taylor
-              </span>
-            </a>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
-              </svg>
+    <header>
+      <div className="fixed top-0 left-0 w-full backdrop-blur-md mix-blend-difference">
+        <div className="container !max-w-full">
+          <div className="flex justify-between h-20 items-center">
+            <div>
+              <a href="/">
+                <span className="text-xl font-bold uppercase text-white">
+                  Alex&nbsp; Taylor
+                </span>
+              </a>
             </div>
-            <div className="hidden md:inline-flex">
-            <Button variant="primary" className="hidden md:inline-flex">
-              Contact Me
-            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="fixed top-0 left-0 w-full">
+        <div className="container !max-w-full">
+          <div className="flex justify-end h-20 items-center">
+            <div className="flex items-center gap-4">
+              <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <motion.rect
+                    x="3"
+                    y="15"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={bottomLineScope}
+                    style={{
+                      transformOrigin: "12px 16px",
+                    }}
+                  />
+                  <motion.rect
+                    x="3"
+                    y="7"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={topLineScope}
+                    style={{
+                      transformOrigin: "12px 8px",
+                    }}
+                  />
+                </svg>
+              </div>
+              <div className="hidden md:inline-flex">
+                <Button variant="primary" className="hidden md:inline-flex">
+                  Contact Me
+                </Button>
+              </div>
             </div>
           </div>
         </div>
